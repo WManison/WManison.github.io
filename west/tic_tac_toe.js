bd1b4e21208c36528714ce5f5ac1a7bbf66da718
WManison <157060583+WManison@users.noreply.github.com>
2025-04-08 17:00:13 -0600

    game is functional (updates to tic_tac_toe.{css,html,js}

---
+++ west/tic_tac_toe.js
@@ -0,0 +1,86 @@
+/*
+    West Manison
+    Last edited: 4/7/2025
+    Description: adds interactivity with tic tac toe game. must check for specific winning combinations
+    and return appropriate game-ending message.
+*/
+
+// create constants
+const board = document.getElementById('game')
+const squares = document.getElementsByClassName('cell')
+const players = ['X', 'O']
+let currentPlayer = players[0] // player 1 = X
+const endMessage = document.createElement('h2')
+    endMessage.textContent = "X's turn !"
+    endMessage.style.marginTop = '30px'
+    endMessage.style.textAlign='center'
+board.after(endMessage)
+
+// winning combinations
+const winning_combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+// 
+for(let i = 0; i < squares.length; i++){
+    squares[i].addEventListener('click', () => {
+        if(squares[i].textContent !== ''){
+            return
+        }
+        squares[i].textContent = currentPlayer
+        squares[i].classList.add(currentPlayer === 'X' ? 'player-x' : 'player-o');
+        if(checkWin(currentPlayer)) {
+            endMessage.textContent=`Game over!\n${currentPlayer} wins!`
+            endMessage.style.whiteSpace = "pre-line"           
+            return
+        }
+        if(checkTie()) {
+            endMessage.textContent= `Game is tied!`
+            return
+        }
+        currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0] 
+        if(currentPlayer == players[0]) {
+            endMessage.textContent= `X's turn!`
+        } else {
+            endMessage.textContent= `O's turn!`
+        }     
+    })   
+}
+
+// CHECK WIN.  verify whether any current combinations result in a win
+function checkWin(currentPlayer) {
+    for(let i = 0; i < winning_combinations.length; i++){
+        const [a, b, c] = winning_combinations[i]
+        if(squares[a].textContent === currentPlayer && squares[b].textContent === currentPlayer && squares[c].textContent === currentPlayer){
+            return true
+        }
+    }
+    return false
+}
+
+// CHECK TIE. verify whether all cells are filled without matching winning combination (a tie)
+function checkTie(){
+    for(let i = 0; i < squares.length; i++) {
+        if(squares[i].textContent === '') {
+            return false;
+        }
+    }
+    return true
+}
+
+// RESTART BUTTON. adds functional restart button
+function restartButton() {
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].textContent = ""
+        squares[i].classList.remove('player-x', 'player-o');
+    }
+    endMessage.textContent=`X's turn!`
+    currentPlayer = players[0]
+}
